79617287d6b100f5ebad753c57e74d3bacbc25dd
Jan Fic <jan.fic18@ncf.edu>
2021-09-03 22:38:43 -0400

    finished main play screen background

--- computercombat/core/src/com/janfic/games/computercombat/screens/PlayScreen.java
+++ computercombat/core/src/com/janfic/games/computercombat/screens/PlayScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
