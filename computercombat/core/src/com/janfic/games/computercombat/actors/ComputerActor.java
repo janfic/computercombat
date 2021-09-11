a59092ae10f123e274486a477bfd7604628cff1c
Jan Fic <jan.fic18@ncf.edu>
2021-09-11 12:07:35 -0400

    animation work

--- computercombat/core/src/com/janfic/games/computercombat/actors/ComputerActor.java
+++ computercombat/core/src/com/janfic/games/computercombat/actors/ComputerActor.java
@@ -1,8 +1,10 @@
 package com.janfic.games.computercombat.actors;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 /**
  *
@@ -15,7 +17,7 @@ public class ComputerActor extends Panel {
     public ComputerActor(Skin skin) {
         super(skin);
 
-        this.defaults().space(1).width(72);
+        this.defaults().space(1);
 
         ProgressBar.ProgressBarStyle green = new ProgressBar.ProgressBarStyle(skin.get("default-horizontal", ProgressBar.ProgressBarStyle.class));
         green.knobBefore = skin.newDrawable("progress_bar_before_vertical", Color.valueOf("9cdb43"));
@@ -23,14 +25,22 @@ public class ComputerActor extends Panel {
         ProgressBar.ProgressBarStyle blue = new ProgressBar.ProgressBarStyle(skin.get("default-horizontal", ProgressBar.ProgressBarStyle.class));
         blue.knobBefore = skin.newDrawable("progress_bar_before_vertical", Color.valueOf("249fde"));
 
+        Table table = new Table();
+
         healthBar = new ProgressBar(0, 10, 1, false, green);
         magicBar = new ProgressBar(0, 10, 1, false, blue);
 
         healthBar.setValue(5);
         magicBar.setValue(7);
 
-        this.add(healthBar).row();
-        this.add(magicBar);
+        Panel panel = new Panel(skin);
+        panel.add(new Label("7", skin));
+
+        table.add(healthBar).width(40).row();
+        table.add(magicBar).width(40);
+
+        this.add(panel).pad(2).width(20);
+        this.add(table);
     }
 
 }
