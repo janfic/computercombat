989eac2f48e193e89bb6c353f17abfeb02962581
Jan Fic <jan.fic18@ncf.edu>
2022-02-10 10:30:52 -0500

    added mystery pack art

--- computercombat/core/src/com/janfic/games/computercombat/screens/OpenPackScreen.java
+++ computercombat/core/src/com/janfic/games/computercombat/screens/OpenPackScreen.java
@@ -24,88 +24,88 @@ import com.janfic.games.computercombat.model.Collection;
  * @author Jan Fic
  */
 public class OpenPackScreen implements Screen {
-
+    
     ComputerCombatGame game;
-
+    
     Stage stage;
     Camera camera;
     Skin skin;
-
+    
     Collection collection;
     CollectionPackActor pack;
-
+    
     public OpenPackScreen(ComputerCombatGame game, Collection collection) {
         this.game = game;
         this.collection = collection;
     }
-
+    
     @Override
     public void show() {
         this.camera = new OrthographicCamera(1920 / 4, 1080 / 4);
         this.stage = ComputerCombatGame.makeNewStage(camera);
         this.skin = game.getAssetManager().get(Assets.SKIN);
-
+        
         this.pack = new CollectionPackActor(game, skin, collection);
-
+        
         Gdx.input.setInputProcessor(stage);
-
+        
         Table table = new Table();
         table.setFillParent(true);
         table.defaults().space(20);
-
+        
         TextButton openButton = new TextButton("Open!", skin);
-        openButton.setVisible(false);
+        openButton.addAction(Actions.fadeOut(0));
         openButton.setTouchable(Touchable.disabled);
-
+        
         table.add(pack).row();
         table.add(openButton).width(100).row();
-
+        
         Action cardAction = Actions.sequence(Actions.visible(false), Actions.delay(2), Actions.moveBy(0, -stage.getHeight() * 2), Actions.visible(true),
                 Actions.moveBy(0, stage.getHeight() * 2, 2, Interpolation.fastSlow),
                 Actions.delay(2));
-
-        Action buttonAction = Actions.sequence(Actions.delay(6), Actions.visible(true), Actions.touchable(Touchable.enabled));
-
+        
+        Action buttonAction = Actions.sequence(Actions.delay(5), Actions.fadeIn(1), Actions.touchable(Touchable.enabled));
+        
         openButton.addAction(buttonAction);
         pack.addAction(cardAction);
-
+        
         openButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.popScreen();
             }
         });
-
+        
         stage.addActor(table);
         table.layout();
     }
-
+    
     @Override
     public void render(float delta) {
         stage.act(delta);
         stage.draw();
     }
-
+    
     @Override
     public void resize(int width, int height) {
         this.stage.getViewport().update(width, height);
         camera.update();
     }
-
+    
     @Override
     public void pause() {
     }
-
+    
     @Override
     public void resume() {
     }
-
+    
     @Override
     public void hide() {
     }
-
+    
     @Override
     public void dispose() {
     }
-
+    
 }
