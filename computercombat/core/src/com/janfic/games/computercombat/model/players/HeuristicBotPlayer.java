804ede870c2e1c9c8981f2e5e9c6d7b39b64c40f
Jan Fic <jan.fic18@ncf.edu>
2022-02-22 09:40:45 -0500

    added average move score to reduce impact of indeterminism

--- computercombat/core/src/com/janfic/games/computercombat/model/players/HeuristicBotPlayer.java
+++ computercombat/core/src/com/janfic/games/computercombat/model/players/HeuristicBotPlayer.java
@@ -7,11 +7,7 @@ import com.janfic.games.computercombat.model.match.MatchResults;
 import com.janfic.games.computercombat.model.match.MatchState;
 import com.janfic.games.computercombat.model.moves.Move;
 import com.janfic.games.computercombat.model.moves.MoveResult;
-import com.janfic.games.computercombat.model.players.heuristicanalyzers.ChargedAbilitiesHeuristicAnalyzer;
-import com.janfic.games.computercombat.model.players.heuristicanalyzers.ComponentsCollectedHeuristicAnalyzer;
-import com.janfic.games.computercombat.model.players.heuristicanalyzers.DrewCardHeuristicAnalyzer;
-import com.janfic.games.computercombat.model.players.heuristicanalyzers.ExtraTurnHeuristicAnalyzer;
-import com.janfic.games.computercombat.model.players.heuristicanalyzers.HeuristicAnalyzer;
+import com.janfic.games.computercombat.model.players.heuristicanalyzers.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -27,6 +23,8 @@ public class HeuristicBotPlayer extends Player {
 
     List<HeuristicAnalyzer> priorityList;
 
+    private final int MOVE_TRIES = 3;
+
     public HeuristicBotPlayer() {
     }
 
@@ -38,7 +36,7 @@ public class HeuristicBotPlayer extends Player {
     public void beginMatch(MatchState state, Player opponent) {
         this.currentState = state;
         this.priorityList = new ArrayList<>();
-        this.priorityList.add(new ChargedAbilitiesHeuristicAnalyzer());
+        this.priorityList.add(new ExtraTurnHeuristicAnalyzer());
         this.priorityList.add(new DrewCardHeuristicAnalyzer());
     }
 
@@ -48,20 +46,25 @@ public class HeuristicBotPlayer extends Player {
         Collections.shuffle(moves);
 
         for (Move move : moves) {
-            List<MoveResult> results = GameRules.makeMove(currentState, move);
-            double totalScore = 0;
-            for (int i = 0; i < priorityList.size(); i++) {
-                HeuristicAnalyzer analyzer = priorityList.get(i);
-                double baseScore = analyzer.analyze(results);
-                double priorityScalar = Math.pow(2, i);
-                double priorityScore = priorityScalar * baseScore;
-                totalScore += priorityScore;
+            double moveSum = 0;
+            // Repeat move and find average score
+            for (int j = 0; j < MOVE_TRIES; j++) {
+                List<MoveResult> results = GameRules.makeMove(currentState, move);
+                double totalScore = 0;
+                for (int i = 0; i < priorityList.size(); i++) {
+                    HeuristicAnalyzer analyzer = priorityList.get(i);
+                    double baseScore = analyzer.analyze(results);
+                    double priorityScalar = Math.pow(2, i);
+                    double priorityScore = priorityScalar * baseScore;
+                    totalScore += priorityScore;
+                }
+                moveSum += totalScore;
             }
-            move.setValue(totalScore);
+            double moveAverage = moveSum / MOVE_TRIES;
+            move.setValue(moveAverage);
         }
 
         moves.sort(new MoveValueComparator());
-
         return moves.get(0);
     }
 
